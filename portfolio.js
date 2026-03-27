6edac507aa76566729762967a04ffe0cd92370f1
Oddunayo <basitobadara6@gmail.com>
2026-03-27 03:30:23 +0100

    made noticeable edits

--- portfolio.js
+++ portfolio.js
@@ -1,76 +1,38 @@
-<<<<<<< HEAD
 //toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
-}
-
-
-//scroll sections
-let sections = document.querySelectorAll('section')
-let navalinks = document.querySelectorAll('header nav a');
-
-
-
-window.onscroll= () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            //active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
-            });
-        }
-    })
-
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-=======
-//toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
-}
-
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 //scroll sections
-let sections = document.querySelectorAll('section')
-let navalinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
+    if (top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-window.onscroll= () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  let header = document.querySelector("header");
 
-        if(top >= offset && top < offset + height) {
-            //active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
-            });
-        }
-    })
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
->>>>>>> 1d59290e336639b77b75a2e433ca436372032f8c
-}
+  menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+};
